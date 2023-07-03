cbd4506c5ca3b87285ff3de167762a76647d901e
JeongTaek Oh <tei1234tei@gmail.com>
2023-07-03 22:10:25 +0900

    add initial springboot project

---
+++ src/main/java/com/mycouponissuer/restservice/MyCouponIssuerApplication.java
@@ -0,0 +1,13 @@
+package com.mycouponissuer.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCouponIssuerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCouponIssuerApplication.class, args);
+	}
+
+}
