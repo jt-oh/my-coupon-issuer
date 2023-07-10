586e6b8a628fd7b2d6f6e66b2fa00ae27c7d9673
JeongTaek Oh <tei1234tei@gmail.com>
2023-07-11 00:18:13 +0900

    change RootController name and add host info to greeting

--- src/main/java/com/mycouponissuer/restservice/RootController.java
+++ src/main/java/com/mycouponissuer/restservice/RootController.java
@@ -1,5 +1,6 @@
 package com.mycouponissuer.restservice;
 
+import java.net.InetAddress;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +10,17 @@ class RootApiController {
 
     @GetMapping("/")
     public String index(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        String hostname;
+
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+
+            hostname = inetAddress.toString();
+        } catch (Exception e) {
+            hostname = "unknown";
+        }
+
+        return String.format("Hello %s! Greeting from %s", name, hostname);
     }
 
 }
