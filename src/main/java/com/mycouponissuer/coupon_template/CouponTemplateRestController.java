d9a7d40e38eeefe5f9fc528917e704212d853ce4
JeongTaek Oh <tei1234tei@gmail.com>
2023-08-20 20:33:48 +0900

    implement coupon template rest controller

---
+++ src/main/java/com/mycouponissuer/coupon_template/CouponTemplateRestController.java
@@ -0,0 +1,35 @@
+package com.mycouponissuer.coupon_template;
+
+import com.mycouponissuer.coupon_template.application.CouponTemplateApplicationService;
+import com.mycouponissuer.coupon_template.application.dto.CouponDTO;
+import com.mycouponissuer.coupon_template.application.dto.CouponIssueRequest;
+import com.mycouponissuer.coupon_template.application.dto.CouponTemplateCreateRequest;
+import com.mycouponissuer.coupon_template.application.dto.CouponTemplateDTO;
+import com.mycouponissuer.coupon_template.application.outbound_ports.CouponRepository;
+import com.mycouponissuer.coupon_template.application.outbound_ports.CouponTemplateRepository;
+import com.mycouponissuer.coupon_template.domain.CouponTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController()
+@RequestMapping("api/v1/coupon-template")
+public class CouponTemplateRestController {
+    @Autowired
+    private CouponTemplateApplicationService couponTemplateApplicationService;
+    @Autowired
+    private CouponTemplateRepository couponTemplateRepository;
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @PostMapping
+    public CouponTemplateDTO createCouponTemplate(@RequestBody CouponTemplateCreateRequest createRequest) {
+        return couponTemplateApplicationService.createCouponTemplate(createRequest);
+    }
+
+    @GetMapping
+    public ArrayList<CouponTemplateDTO> getCouponTemplateList() {
+        return couponTemplateApplicationService.getCouponTemplateList();
+    }
+}
